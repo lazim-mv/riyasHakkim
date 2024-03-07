0c46d781b0c495f3ea821d9185898f62154d0ce7
lazim mv <lazimlatheef@gmail.com>
2024-03-07 19:25:46 +0530

    v0.0.4 MaintainancePage

--- src/app/components/Testimonial.js
+++ src/app/components/Testimonial.js
@@ -7,6 +7,7 @@ import {
 } from "./ButtonComponent";
 import { testimonial } from "../contents/contents";
 import Image from "next/image";
+import { useWindowSize } from "../utils/windowSize";
 
 const Testimonial = ({mobile}) => {
   const cardData = testimonial.testimonialData;
